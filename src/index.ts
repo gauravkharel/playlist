bbf4277ba8ca0a5e0047a91009423bfed2866f43
gauravkharel <kharel619@gmail.com>
2023-08-31 15:48:06 +0545

    adding more routes with all HTTP method

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,87 @@ app.get('/artists', async(req, res) => {
     })
 })
 
+app.get('/playlist',async (req, res) => {
+    const songs = await prisma.song.findMany({
+        where: {released: true},
+        include: {singer: true}
+    })
+
+    res.json({
+        success: true,
+        payload: songs,
+    })
+})
+
+app.get(`/song/:id`,async (req, res) => {
+    const {id} = req.params
+    const song = await prisma.song.findFirst({
+        where: {id: Number(id)}
+    })
+    res.json({
+        success: true,
+        payload: song,
+    })
+})
+
+app.post(`/artist`,async (req,res) => {
+    const result = await prisma.artist.create({
+        data: {...req.body}
+    })
+    res.json({
+        success: true,
+        payload: result
+    })
+})
+
+app.post(`/song`,async (req, res) => {
+    const {title, content, singerEmail} = req.body
+    const result = await prisma.song.create({
+        data: {
+            title,
+            content,
+            released: false,
+            singer: {connect: {email: singerEmail}}
+        }
+    })
+    res.json({
+        success: true,
+        payload: result
+    })
+})
+
+app.put(`/song/release/:id`,async (req, res) => {
+    const {id} = req.params
+    const song = await prisma.song.update({
+        where: {id: Number(id)},
+        data: {released: true}
+    })
+    res.json({
+        success: true,
+        payload: song,
+    })
+})
+
+app.delete(`/song/:id`,async (req, res) => {
+    const {id} = req.params
+    const song = await prisma.song.delete({
+        where: {id: Number(id)}
+    })
+    res.json({
+        success: true,
+        payload: song
+    })
+})
+
+app.get('/artists',async (req,res) => {
+    res.status(404)
+    return res.json({
+        success: false,
+        payload: null,
+        message: `API Response: Path not found: ${req.path}`
+    })
+})
+
 //adding the first route
 app.use((req, res, next) => {
     res.status(404);
